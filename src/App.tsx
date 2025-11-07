a026011ed6868bbec282b34802882d8e15cd7c12
Cyri0 <nemestamas94@gmail.com>
2025-11-07 09:43:54 +0100

    Update README with geocoding sample for Debrecen, add axios dependency, and enhance App component for place search functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,36 @@
+import axios from "axios"
+import { useState } from "react"
+
+type Place = {
+  name: string,
+  country: string
+}
+
 const App = () => {
+  const [place, setPlace] = useState("")
+
+  const [responsePlaces, setResponsePlaces] = useState<Place[]>([])
+
+  const searchPlace = () => {
+    if(place.length === 0) return
+
+    axios.get("https://geocoding-api.open-meteo.com/v1/search?name=" + place)
+    .then(response => setResponsePlaces(response.data.results))
+  }
+
   return (
     <div>
-      Units
-
-      Switch to Imperial/Metric
-
-      Temperature
-
-      Celsius (°C)
-      Fahrenheit (°F)
-
-      Wind Speed
-
-      km/h
-      mph
-
-      Precipitation
-
-      Millimeters (mm)
-      Inches (in)
-
-      How's the sky looking today?
-
-      Search for a city, e.g., New York
-      Search
+      <h1>How's the sky looking today?</h1>
+      <div>
+        <input type="text" 
+               placeholder="Search for a place..."
+               value={place}
+               onChange={(e) => setPlace(e.target.value)}
+        />
+        <button onClick={searchPlace}>Search</button>
+        
+        {responsePlaces.map(resPlace => <button>{resPlace.name} ({resPlace.country})</button>)}
+      </div>
 
       Feels like
 
