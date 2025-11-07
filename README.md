27a374b123de91f476ad9f6acc0be8d0879224f7
Cyri0 <nemestamas94@gmail.com>
2025-11-07 08:24:48 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Links
+
+## Geocoding Sample
+https://geocoding-api.open-meteo.com/v1/search?name=Budapest
+
+## Weather Forecast Sample
+https://api.open-meteo.com/v1/forecast?latitude=47.49835&longitude=19.04045&current_weather=true
---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+const App = () => {
+  return (
+    <div>
+      Units
+
+      Switch to Imperial/Metric
+
+      Temperature
+
+      Celsius (°C)
+      Fahrenheit (°F)
+
+      Wind Speed
+
+      km/h
+      mph
+
+      Precipitation
+
+      Millimeters (mm)
+      Inches (in)
+
+      How's the sky looking today?
+
+      Search for a city, e.g., New York
+      Search
+
+      Feels like
+
+      Humidity
+
+      Wind
+
+      Precipitation
+
+      Daily forecast
+
+      Hourly forecast
+    </div>
+  )
+}
+
+export default App
